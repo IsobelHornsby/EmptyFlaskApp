e37b4fd41b42ad0724521fc7390811ff9445696c
IsobelH <i1hornsby@student.ccn.ac.uk>
2024-01-25 16:35:48 +0000

    Added comments to the files

--- TheApplication/routes.py
+++ TheApplication/routes.py
@@ -1,6 +1,12 @@
+# import the app file
 from __main__ import app
+# import the flask functions
 from flask import url_for, render_template, request, redirect
 
+# the routes
 @app.route('/')
+
+# home page routes
 def home():
     return render_template("home.html")
+
